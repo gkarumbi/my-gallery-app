5c5bcba345373205fb0ff0f34cf73e117029611d
gkarumbi <gkarumbi@gmail.com>
2020-08-03 14:40:02 +0300

    Add Image model to handle creating image objects in our database

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    image_name = models.CharField(max_length=30)
+    image_description = models.CharField(max_length=255)
+    image_location = models.CharField(max_length=30)
+    image_category = models.CharField(max_length = 30)
+    image= models.ImageField(upload_to='images/')

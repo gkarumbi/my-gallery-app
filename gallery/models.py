a5b3f8a10b93f5af2cb59ee187e36327d0126764
gkarumbi <gkarumbi@gmail.com>
2020-08-03 16:08:37 +0300

    Add save_image method to save our images to the database

--- gallery/models.py
+++ gallery/models.py
@@ -5,13 +5,27 @@ from django.db import models
 class Image(models.Model):
     image_name = models.CharField(max_length=30)
     image_description = models.CharField(max_length=255)
-    image_location = models.ForeignKey(Location)
-    image_category = models.ForeignKey(Category)
+    image_location = models.ForeignKey('Location',on_delete=models.CASCADE)
+    image_category = models.ForeignKey('Category',on_delete=models.CASCADE)
     image= models.ImageField(upload_to='images/')
+    
+    def __str__(self):
+        return self.image_name
+
+    @classmethod
+    def save_image(self):
+        self.save()
+
 
 class Location(models.Model):
     location = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.location
+
 
 class Category(models.Model):
     category = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category

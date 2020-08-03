a38d8b8019a15179ac6cacf9ec604c86fd3e1c71
gkarumbi <gkarumbi@gmail.com>
2020-08-03 14:52:49 +0300

    Add Location and Category models to create our location and Category objects

--- gallery/models.py
+++ gallery/models.py
@@ -5,6 +5,13 @@ from django.db import models
 class Image(models.Model):
     image_name = models.CharField(max_length=30)
     image_description = models.CharField(max_length=255)
-    image_location = models.CharField(max_length=30)
-    image_category = models.CharField(max_length = 30)
+    image_location = models.ForeignKey(Location)
+    image_category = models.ForeignKey(Category)
     image= models.ImageField(upload_to='images/')
+
+class Location(models.Model):
+    location = models.CharField(max_length=30)
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=30)

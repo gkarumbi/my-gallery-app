be863dfa0ea0a252e6ee67c7a9d3dc61da18685c
gkarumbi <gkarumbi@gmail.com>
2020-08-03 08:05:31 +0300

    Push initial folder structure

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/george/Documents/moringa-core/Django/my-gallery-app/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

6347e4dbedfcc09fd049bc80c9a749b47b7b05a9
gkarumbi <gkarumbi@gmail.com>
2020-08-04 12:47:04 +0300

    Add urls to our view functions

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,11 +1,15 @@
 from  django.conf import settings
-from django.conf.urls import url
+from django.conf.urls import url,include
+from . import views
+from django.contrib import admin
 from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
-    url('^$', views.welcome, name = 'welcome')
+    url(r'^$', views.my_photos, name = 'my-photos'),
+    url(r'^images/(\d+)', views.my_photo,'my-photo'),
+    url(r'^search/', views.search_results,name='results')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

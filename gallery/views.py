4bc972d6226ccf8bedf7e98b69f488e7d94063ab
gkarumbi <gkarumbi@gmail.com>
2020-08-04 12:36:38 +0300

    Add view function to display our our photos on our homepage

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from .models import Image
+import datetime as dt
 
 # Create your views here.
 
-def welcome(request):
-    return render(request,'welcome.html')
+def my_photos(request):
+    photos = Image.objects.all()
+
+    return render(request,'welcome.html',{'photos':photos})
